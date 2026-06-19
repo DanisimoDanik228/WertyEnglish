2fe2ad5f986108bc0934910b2f777b87036aeceb
danil <danilaorlovic28@gmail.com>
2026-06-19 11:06:00 +0300

    feat: basic struct translate

---
+++ Interface.Services/TranslateService.cs
@@ -0,0 +1,24 @@
+﻿using Application.Options;
+using Application.Services;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Services
+{
+    public class TranslateService : ITranslateService
+    {
+        private readonly TranslateSetting _translateSetting;
+        public TranslateService(IOptions<TranslateSetting> options)
+        {
+            _translateSetting = options.Value;
+        }
+
+        public async Task<string> TranslateWord(string word)
+        {
+            return word.ToUpper() + _translateSetting.AdressLibreTranslate;
+        }
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,5 +1,7 @@
+using Application.Options;
 using Application.Services;
 using Infrastructure.Repositories;
+using Infrastructure.Services;
 using Interface.Services;
 using Repositories;
 
@@ -16,6 +18,10 @@ namespace WertyEnglish
             builder.Services.AddSingleton<IPairWordRepository, InMemoryPairWordRepository>();
 
             builder.Services.AddSingleton<IPairWordService, PairWordService>();
+            builder.Services.AddSingleton<ITranslateService, TranslateService>();
+
+            builder.Services.Configure<TranslateSetting>(
+                builder.Configuration.GetSection("TranslateSetting"));
 
             var app = builder.Build();
 
